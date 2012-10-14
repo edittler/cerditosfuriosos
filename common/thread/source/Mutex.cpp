74277d31b6b4669a38d43259f6e70b5ec82565f4
ezeperez26 <ezeperez26@gmail.com>
2012-10-14 22:04:15 +0000

    Modifico la jerarquia de carpetas de common y añado la clase Mutex

---
+++ common/thread/source/Mutex.cpp
@@ -0,0 +1,26 @@
+/* Implementación de la clase Mutex que encapsula el mutex de la librería
+ * POSIX Thread
+ */
+
+#include "../include/Mutex.h"
+
+Mutex::Mutex() {
+	// Inicializa el mutex POSIX Thread que encapsula.
+	pthread_mutex_init(&(this->m), NULL);
+}
+
+Mutex::~Mutex() {
+	// Destruye el mutex POSIX Thread que encapsula.
+	pthread_mutex_destroy(&(this->m));
+}
+
+void Mutex::lock() {
+	// Bloquea el mutex POSIX Thread que encapsula.
+	pthread_mutex_lock(&(this->m));
+}
+
+void Mutex::unlock() {
+	// Desbloquea el mutex POSIX Thread que encapsula.
+	pthread_mutex_unlock(&(this->m));
+}
+
