23e400492387b9a20f3cc98f4edfb9c5c31daf29
ezeperez26@gmail.com <ezeperez26@gmail.com@76056235-4813-e1b9-e710-aa10b59abfcd>
2012-10-18 22:55:52 +0000

    + Añado los main.cpp para cada aplicacion que unicamente saludan al usuario (vaya funcionalidad!!) + Añado los makefile necesarios para compilar cada directorio (hay q mejorarlos aún, por ejemplo incluir la posibilidad de limpiar todos los directorios desde el makefile global)

---
+++ trunk/designer/main.cpp
@@ -0,0 +1,14 @@
+/* Aplicación del diseñador
+ *
+ */
+
+// Standard C++ Library Includes
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	std::cout << "Soy el Diseñador!!" << std::endl;
+
+	return 0;
+}
+
+
