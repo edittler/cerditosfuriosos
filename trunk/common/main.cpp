821e8c97d21326adbdfae1ee9712a9c7ca8894ea
matias.cerrotta@gmail.com <matias.cerrotta@gmail.com@76056235-4813-e1b9-e710-aa10b59abfcd>
2012-10-27 00:07:28 +0000

    actualizo para ejemplos de colision

--- trunk/common/main.cpp
+++ trunk/common/main.cpp
@@ -14,16 +14,20 @@ int main(int argc, char *argv[]) {
 //	escena.agregarCajaMadera(1,1);
 
 	// choque en y de pajaro y huevo (en 4ta iteracion)
-//	escena.lanzarPajaroRojo(10, 15, 0, -20);
-//	escena.lanzarHuevoBlanco(10, 10, 0, 20);
+	escena.lanzarPajaroRojo(10, 15, 0, -20);
+	escena.lanzarHuevoBlanco(10, 12, 0, 20);
 
 	// choque de dos pajaros (en 5ta iteracion)
-	escena.lanzarPajaroRojo(5, 3, 20, 30);
-	escena.lanzarPajaroRojo(9, 3, -20, 30);
+//	escena.lanzarPajaroRojo(5, 3, 20, 30);
+//	escena.lanzarPajaroRojo(9, 3, -20, 30);
 
-	// restitucion - se calcula
+	// restitucion - se calcula segun la velocidad y parametro de restitucion
 //	escena.lanzarPajaroRojo(5, 2, 0, -20);
-//	escena.lanzarPajaroRojo(10, 0.5, 0, 0);
+//	escena.lanzarPajaroRojo(10, 0.9, 0, 0);
+
+	// choque con bordes (en 2ra iteracion)
+//	escena.lanzarHuevoBlanco(5, 3, 0, 10);
+//	escena.lanzarPajaroRojo(5.5, 5, 0, -10);
 
 	for(int i = 0; i < 200; i++) {
 		escena.correrTick();
