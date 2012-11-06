9f7e80059072aa481a4c8e5ac32a5b3c66b28a1a
lauta1608@gmail.com <lauta1608@gmail.com@76056235-4813-e1b9-e710-aa10b59abfcd>
2012-11-06 04:06:30 +0000

    Cambios a los niveles

--- trunk/common/model/NivelLocal.cpp
+++ trunk/common/model/NivelLocal.cpp
@@ -1,4 +1,5 @@
 #include "NivelLocal.h"
+#include "Constantes.h"
 
 NivelLocal::NivelLocal() {
 	// TODO Auto-generated constructor stub
@@ -8,3 +9,26 @@ NivelLocal::~NivelLocal() {
 	// TODO Auto-generated destructor stub
 }
 
+
+void NivelLocal::tick(int milisegundos) {
+	// TODO hacer que la generacion de pajaros sea segun el tiempo seteado
+	this->generarPajaro();
+	escenario->correrTick();
+}
+
+void NivelLocal::generarPajaro() {
+	int res = simulador->generarPajaro();
+
+	// TODO setear bien los valores, la posicion debe ser aleatoria
+	switch (res) {
+		case PAJARO_AZUL:
+			escenario->lanzarPajaroAzul(Punto2D(0,50), Velocidad2D(5,-5));
+			break;
+		case PAJARO_ROJO:
+			escenario->lanzarPajaroRojo(Punto2D(0,50), Velocidad2D(5,-5));
+			break;
+		case PAJARO_VERDE:
+			escenario->lanzarPajaroVerde(Punto2D(0,50), Velocidad2D(5,-5));
+			break;
+	}
+}
