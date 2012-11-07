46efc5e66fab4e6bef72079b0f9c2ecec055c664
matias.cerrotta@gmail.com <matias.cerrotta@gmail.com@76056235-4813-e1b9-e710-aa10b59abfcd>
2012-11-07 19:59:26 +0000

    * Refactorizacion. Muevo funcionalidad de constructor Vistas a constructor VistaCuerpo

--- trunk/client/vista/modelo/VistaPajaroRojo.cpp
+++ trunk/client/vista/modelo/VistaPajaroRojo.cpp
@@ -13,11 +13,6 @@ VistaPajaroRojo::VistaPajaroRojo(VistaEscenario* escenario,
 {
 	// Registro el observador en el modelo.
 	pajaro->registrarObservador(this);
-	// Obtengo la posicion del cuerpo y realizo el ajuste de valores.
-	Punto2D p = pajaro->getPosicion();
-	this->x = this->ajustarValorX(p.x);
-	this->y = this->ajustarValorY(p.y);
-	this->escenario->put(*this, this->x, this->y);
 	// Obtengo la velocidad en x y selecciono la imagen a usar
 	float velX = pajaro->getVelocidad().x;
 	if (velX >= 0) {
