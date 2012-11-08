0395e94ec7d9a682ceda1d76dd5dca2ec7eae6bd
matias.cerrotta <matias.cerrotta@gmail.com>
2012-11-08 15:15:58 +0000

    * Implemento huevo codorniz

--- client/main.cpp
+++ client/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
 	escenario.lanzarPajaroRojo(Punto2D(ancho - 1, alto - 4), Velocidad2D(-12, 1));
 	escenario.lanzarPajaroRojo(Punto2D(ancho - 5, 2), Velocidad2D(-4, 9));
 	escenario.lanzarHuevoBlanco(Punto2D(1, 1), Velocidad2D(5.5, 13));
-	escenario.lanzarHuevosCodorniz(Punto2D(alto - 1, 1), Velocidad2D(7, -8));
+	escenario.lanzarHuevosCodorniz(Punto2D(1, alto - 1), Velocidad2D(7, 2));
 	escenario.lanzarHuevoReloj(Punto2D(3, 1), Velocidad2D(3, 7));
 
 	SimuladorProvisorio sim(&escenario);
