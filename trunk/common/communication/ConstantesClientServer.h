3ac95440c04e1e00f788fe9f5e215790e496cb96
matias.cerrotta@gmail.com <matias.cerrotta@gmail.com@76056235-4813-e1b9-e710-aa10b59abfcd>
2012-12-04 03:51:20 +0000

    * Ajusto constantes

--- trunk/common/communication/ConstantesClientServer.h
+++ trunk/common/communication/ConstantesClientServer.h
@@ -7,7 +7,7 @@ typedef unsigned short int Puerto;
 #define PUERTO_DEFAULT (Puerto) 5555
 #define MAX_CONEXION_ESPERA 10
 
-#define MAX_MSJ_PROCESADOS 10
+#define MAX_MSJ_PROCESADOS 40
 
 // Cliente
 #define SERVER_IP_DEFAULT "127.0.0.1"
